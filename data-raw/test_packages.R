7906b35714caac09c73bb4bf98b8c3a1bcbf5d67
Robert DJ <git@dahl-jacobsen.dk>
2019-10-30 20:20:54 +0100

    :white_check_mark: Adding tests

---
+++ data-raw/test_packages.R
@@ -0,0 +1,17 @@
+create_empty_package <- function(package_name, version) {
+    package_path <- file.path(tempdir(), package_name)
+    dir.create(package_path)
+    usethis::create_package(package_path, rstudio = FALSE, open = FALSE)
+    unlink(file.path(package_path, "R"), recursive = TRUE)
+
+    package_desc <- desc::desc(file = file.path(package_path, "DESCRIPTION"))
+    package_desc$set_version(version)
+    package_desc$write()
+
+    devtools::build(pkg = package_path)
+}
+
+file.copy(create_empty_package("foo", "0.0.1"), here::here("inst", "testdata"), overwrite = TRUE)
+file.copy(create_empty_package("foo", "0.0.2"), here::here("inst", "testdata"), overwrite = TRUE)
+
+file.copy(create_empty_package("bar", "0.0.1"), here::here("inst", "testdata"), overwrite = TRUE)
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(cranitor)
+
+test_check("cranitor")
