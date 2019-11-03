e4f9fb7bda6d0ca33184f61803eb59eedb7475ff
Robert DJ <git@dahl-jacobsen.dk>
2019-11-03 16:37:13 +0100

    :pencil: Reformat docs

--- R/cranitor-package.R
+++ R/cranitor-package.R
@@ -1,6 +1,7 @@
 #' `cranitor` package
 #'
-#' Taking care of a CRAN from initialization to updating metadata when new packages/package versions are included.
+#' Taking care of a CRAN from initialization to updating metadata when new packages/package versions
+#' are included.
 #'
 #' See the README on [GitHub](https://github.com/robertdj/cranitor).
 #'
