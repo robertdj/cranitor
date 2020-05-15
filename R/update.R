9df0022d87b3281a5057a6526282100094f0c314
Robert DJ <git@dahl-jacobsen.dk>
2020-05-15 18:33:23 +0200

    Get DESCRIPTION file from compressed archive

--- R/update.R
+++ R/update.R
@@ -25,7 +25,50 @@ update_cran_source <- function(cran_root, targz_file) {
 }
 
 
+get_package_desc <- function(package_file) {
+    package_name <- package_name_from_filename(basename(package_file))
+
+    if (package_ext(package_file) == "zip") {
+        extractor <- unzip
+    } else {
+        extractor <- untar
+    }
+
+
+    tmp_dir <- fs::path_temp(package_name)
+    # Move to separate function and use defer_parent
+    withr::defer(fs::dir_delete(tmp_dir))
+
+    files_in_tar <- extractor(package_file, list = TRUE)
+    # unzip returns a dataframe. untar returns a vector
+    if (is.data.frame(files_in_tar))
+        files_in_tar <- files_in_tar$Name
+
+    desc_location <- fs::path(package_name, "DESCRIPTION")
+    desc_exists_in_tar <- any(desc_location == files_in_tar)
+
+    if (isFALSE(desc_exists_in_tar))
+        stop("DESCRIPTION is not in expected location in package file")
+
+    extractor(package_file, files = desc_location, exdir = tmp_dir)
+
+    desc_file <- fs::path(tmp_dir, desc_location)
+
+
+    desc <- tryCatch(read.dcf(desc_file), error = identity)
+
+    if ((inherits(desc, "error")) || (length(desc) == 0))
+        stop("Malformed DESCRIPTION in ", package_file)
+
+    return(desc)
+}
+
+
 update_cran_win <- function(cran_root, zip_file) {
+    print("win")
+    desc <- get_package_desc(zip_file)
+    meta <- get_package_meta(zip_file)
+
     import_win_package(cran_root, zip_file)
 
     # TODO: basename_from_targz is not a good name
