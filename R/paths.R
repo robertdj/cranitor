4f5cbd09eec00316c16e37651de767c3124e92d9
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-05-21 20:50:37 +0200

    Don't document too many internal functions

--- R/paths.R
+++ R/paths.R
@@ -1,15 +1,8 @@
-#' The path of the source package folder
-#'
-#' @inheritParams update_cran
 source_package_dir <- function(cran_root) {
     fs::path(cran_root, "src", "contrib")
 }
 
 
-#' The path of the Windows package folder
-#'
-#' @inheritParams update_cran
-#' @param r_version An `R_system_version` -- the result of e.g. [getRversion()].
 win_package_dir <- function(cran_root, r_version = getRversion()) {
     assertthat::assert_that(
         inherits(r_version, "R_system_version")
@@ -35,30 +28,17 @@ list_win_package_dirs <- function(cran_root) {
 
 
 
-#' The path of the Mac package folder
-#'
-#' @inheritParams update_cran
 mac_package_dir <- function(cran_root) {
     # TODO: macOS name?
     fs::path(cran_root, "bin", "macosx", "contrib")
 }
 
 
-#' The path of the archive metadata
-#'
-#' @inheritParams update_cran
-#'
-#' @return The path of `archive.rds` relative to `cran_root`
 archive_metadata_path <- function(cran_root) {
     fs::path(source_package_dir(cran_root), "Meta", "archive.rds")
 }
 
 
-#' The path of the Archive folder
-#'
-#' @inheritParams update_cran
-#'
-#' @return The path of the `Archive` folder relative to `cran_root`
 archive_path <- function(cran_root) {
     fs::path(source_package_dir(cran_root), "Archive")
 }
--- R/source.R
+++ R/source.R
@@ -21,13 +21,6 @@ update_cran_source <- function(cran_root, targz_file) {
 }
 
 
-#' Import source package
-#'
-#' Import source package `tar.gz` file into a local CRAN by copying the file into the
-#' appropriate folder.
-#'
-#' @inheritParams update_cran
-#' @param package A vector with filenames (including path) of the `tar.gz` files.
 import_source_package <- function(cran_root, targz_file) {
     assertthat::assert_that(
         package_ext(targz_file) == "tar.gz"
