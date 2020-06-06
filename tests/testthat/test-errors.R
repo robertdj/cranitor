a53ac9277aaf4c1633ffeeec6c25260fa9b02a67
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-06-06 21:00:02 +0200

    Test error handling

---
+++ tests/testthat/test-errors.R
@@ -0,0 +1,36 @@
+
+test_that("Import non-package archive", {
+    tmp_file <- fs::file_create(fs::file_temp())
+    writeLines("foobar\n", con = tmp_file)
+
+    targz_file <-  fs::file_temp(pattern = "foo_", ext = "tar.gz")
+    # TODO: Change WD to quiet message from tar
+    utils::tar(tarfile = targz_file, files = tmp_file, compression = "gzip", compression_level = 9L, tar = "tar")
+
+    expect_error(
+        update_cran(fs::path_temp("demo_cran"), targz_file),
+        "foo/DESCRIPTION does not exist"
+    )
+})
+
+
+test_that("Import archive with corrupted DESCRIPTION", {
+    wd <- getwd()
+    withr::defer(setwd(wd))
+
+    tmp_dir <- fs::path_temp()
+    fs::dir_create(fs::path(tmp_dir, "foo"))
+    tmp_file <- fs::file_create(fs::path(tmp_dir, "foo", "DESCRIPTION"))
+    writeLines("foobar", con = tmp_file)
+
+    setwd(tmp_dir)
+    targz_file <-  fs::file_temp(pattern = "foo_", ext = "tar.gz")
+    # utils::tar(tarfile = targz_file, files = tmp_file, compression = "gzip")
+    utils::tar(tarfile = targz_file, files = fs::path("foo", "DESCRIPTION"), compression = "gzip", compression_level = 9L, tar = "tar")
+
+    fs::dir_delete(fs::path(tmp_dir, "foo"))
+    expect_error(
+        update_cran(fs::path_temp("demo_cran"), targz_file),
+        "Malformed DESCRIPTION"
+    )
+})
