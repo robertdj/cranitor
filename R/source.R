ccfd13991068bbdac928cb482c80607ab5d97d06
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-21 19:53:13 +0100

    Move input checks

--- R/source.R
+++ R/source.R
@@ -1,11 +1,4 @@
 update_cran_source <- function(targz_file, cran_root) {
-    assertthat::assert_that(
-        assertthat::is.string(cran_root),
-        fs::is_file(targz_file),
-        package_ext(targz_file) == "tar.gz",
-        !pkg.peek::is_package_built(targz_file)
-    )
-
     import_source_package(targz_file, cran_root)
 
     clean_cran_source(cran_root)
@@ -16,8 +9,10 @@ update_cran_source <- function(targz_file, cran_root) {
 
 import_source_package <- function(targz_file, cran_root) {
     assertthat::assert_that(
+        assertthat::is.string(cran_root),
         fs::is_file(targz_file),
-        package_ext(targz_file) == "tar.gz"
+        package_ext(targz_file) == "tar.gz",
+        !pkg.peek::is_package_built(targz_file)
     )
 
     source_dir <- source_package_dir(cran_root)
