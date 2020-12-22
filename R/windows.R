370a08738c0f22dc3c37a717f022930a29defa3d
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-22 13:29:40 +0100

    🪟 Don't pass R version as argument

--- R/windows.R
+++ R/windows.R
@@ -1,34 +1,35 @@
 update_cran_win <- function(zip_file, cran_root) {
     assertthat::assert_that(
         assertthat::is.string(cran_root),
-        assertthat::is.string(zip_file),
-        package_ext(zip_file) == "zip"
+        fs::is_file(zip_file),
+        assertthat::has_extension(zip_file, "zip")
     )
 
     meta <- pkg.peek::get_package_meta(zip_file)
-
     if (tolower(meta$Built$OStype) != "windows")
         stop(zip_file, " not built on Windows")
 
-    r_version_used_in_build <- meta$Built$R
-    import_win_package(zip_file, r_version_used_in_build, cran_root)
+    import_win_package(zip_file, cran_root)
 
     clean_cran_win(cran_root)
 
+    r_version_used_in_build <- pkg.peek::get_r_version(zip_file)
     tools::write_PACKAGES(win_package_dir(r_version_used_in_build, cran_root), type = "win.binary")
 }
 
 
-import_win_package <- function(package, r_version, cran_root) {
+import_win_package <- function(zip_file, cran_root) {
     assertthat::assert_that(
-        assertthat::has_extension(package, "zip")
+        fs::is_file(zip_file),
+        assertthat::has_extension(zip_file, "zip")
     )
 
-    # TODO: Don't check
-    if (isFALSE(fs::dir_exists(win_package_dir(r_version, cran_root))))
-        fs::dir_create(win_package_dir(r_version, cran_root))
+    r_version <- pkg.peek::get_r_version(zip_file)
+    win_dir <- win_package_dir(r_version, cran_root)
+    if (isFALSE(fs::dir_exists(win_dir)))
+        fs::dir_create(win_dir)
 
     # TODO: copy or move?
-    fs::file_copy(package, win_package_dir(r_version, cran_root))
+    fs::file_copy(zip_file, win_dir)
 }
 
