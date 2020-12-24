b0ef2423e99816a8d6139dd0d124706e6a336bec
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-24 13:45:34 +0100

    Similar imports on 🏁 and 🐧

--- R/linux.R
+++ R/linux.R
@@ -6,7 +6,6 @@ update_cran_linux <- function(targz_file, cran_root, distro) {
 
     r_version <- pkg.peek::get_r_version(targz_file)
     linux_dir <- linux_package_dir(r_version, distro, cran_root)
-    # TODO: Check if linux_dir is a valid path
 
     import_linux_package(targz_file, linux_dir)
 
@@ -20,22 +19,16 @@ import_linux_package <- function(targz_file, linux_dir) {
     assertthat::assert_that(
         fs::is_file(targz_file),
         package_ext(targz_file) == "tar.gz",
-        pkg.peek::is_package_built(targz_file)
+        pkg.peek::is_package_built(targz_file),
+        is_valid_linux_dir(linux_dir)
     )
-
-    if (isFALSE(fs::dir_exists(linux_dir))) {
-        path <- tryCatch(
-            fs::dir_create(linux_dir),
-            error = identity
-        )
-
-        if (inherits(path, "identity"))
-            rlang::abort(paste(linux_dir, "is not a valid path"))
-    }
 
     meta <- pkg.peek::get_package_meta(targz_file)
     if (tolower(meta$Built$OStype) != "unix")
         stop(targz_file, " not built on Linux")
+
+    if (isFALSE(fs::dir_exists(linux_dir)))
+        fs::dir_create(linux_dir)
 
     pkg_name <- pkg.peek::get_package_name(targz_file)
     pkg_version <- pkg.peek::get_package_version(targz_file)
--- R/windows.R
+++ R/windows.R
@@ -1,27 +1,31 @@
 update_cran_win <- function(zip_file, cran_root) {
-    import_win_package(zip_file, cran_root)
-
-    archive_win_packages(cran_root)
+    assertthat::assert_that(
+        assertthat::is.string(cran_root)
+    )
 
     r_version <- pkg.peek::get_r_version(zip_file)
-    tools::write_PACKAGES(win_package_dir(r_version, cran_root), type = "win.binary")
+    win_dir <- win_package_dir(r_version, cran_root)
+
+    import_win_package(zip_file, win_dir)
+
+    archive_win_packages(win_dir)
+
+    tools::write_PACKAGES(win_dir, type = "win.binary")
 }
 
 
-import_win_package <- function(zip_file, cran_root) {
+import_win_package <- function(zip_file, win_dir) {
     assertthat::assert_that(
-        assertthat::is.string(cran_root),
         fs::is_file(zip_file),
         assertthat::has_extension(zip_file, "zip"),
-        pkg.peek::is_package_built(zip_file)
+        pkg.peek::is_package_built(zip_file),
+        is_valid_windows_dir(win_dir)
     )
 
     meta <- pkg.peek::get_package_meta(zip_file)
     if (tolower(meta$Built$OStype) != "windows")
         stop(zip_file, " not built on Windows")
 
-    r_version <- pkg.peek::get_r_version(zip_file)
-    win_dir <- win_package_dir(r_version, cran_root)
     if (isFALSE(fs::dir_exists(win_dir)))
         fs::dir_create(win_dir)
 
@@ -29,7 +33,7 @@ import_win_package <- function(zip_file, cran_root) {
 }
 
 
-archive_win_packages <- function(r_version, cran_root, list = FALSE) {
+archive_win_packages <- function(win_dir) {
     win_packages <- fs::dir_ls(win_package_dir(r_version, cran_root), type = "file", glob = "*.zip")
 
     package_names <- package_name_from_filename(win_packages)
